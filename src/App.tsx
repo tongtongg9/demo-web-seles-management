d0ec71f8725f8002615075deae10c69534a29b20
tongtongg9 <kampon.lamyai@gmail.com>
2024-09-30 02:17:45 +0700

    Remove import of random sales data

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/query-client';
 import { AuthProvider } from '@/providers/auth-provider';
 import { ModalProvider } from '@/providers/modal-provider';
-import '@/data/random-sales-data'
 
 export default function App() {
     return (
